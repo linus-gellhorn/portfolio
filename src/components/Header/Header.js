4636c642362838fc0a0556a8259b261b6a0f04a4
Linus Gellhorn <linus.g@hotmail.com>
2022-02-27 11:18:43 +0000

    Making mobile responsive - added hamburger menu for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Brightness2Icon from "@material-ui/icons/Brightness2";
 import WbSunnyRoundedIcon from "@material-ui/icons/WbSunnyRounded";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 import { ThemeContext } from "../../contexts/theme";
 import { header } from "../../content";
 import "./Header.css";
@@ -8,6 +10,9 @@ import "./Header.css";
 const Header = () => {
   const { homepage, title } = header;
   const [{ themeName, toggleTheme }] = useContext(ThemeContext);
+  const [showNavList, setShowNavList] = useState(false);
+
+  const toggleNavList = () => setShowNavList(!showNavList);
 
   return (
     <header className="header">
@@ -17,19 +22,48 @@ const Header = () => {
         </a>
       </h3>
       <nav>
-        <ul>
-          <li>
-            <a href="#projects">Projects</a>
+        <ul
+          className="nav__list"
+          style={{ display: showNavList ? "flex" : null }}
+        >
+          <li className="nav__list-item">
+            <a
+              href="#projects"
+              onClick={toggleNavList}
+              className="link link--nav"
+            >
+              Projects
+            </a>
           </li>
-          <li>
-            <a href="#skills">Skills</a>
+          <li className="nav__list-item">
+            <a
+              href="#skills"
+              onClick={toggleNavList}
+              className="link link--nav"
+            >
+              Skills
+            </a>
           </li>
-          <li>
-            <a href="#contact">Contact</a>
+          <li className="nav__list-item">
+            <a
+              href="#contact"
+              onClick={toggleNavList}
+              className="link link--nav"
+            >
+              Contact
+            </a>
           </li>
         </ul>
         <button type="button" onClick={toggleTheme}>
           {themeName === "light" ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
+        </button>
+        <button
+          type="button"
+          onClick={toggleNavList}
+          className="btn btn--icon nav__hamburger"
+          aria-label="toggle navigation"
+        >
+          {showNavList ? <CloseIcon /> : <MenuIcon />}
         </button>
       </nav>
     </header>
